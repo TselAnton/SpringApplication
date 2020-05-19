0bbfc9854ba8fb11385a851ec477eef25b3a05ac
Tsel Anton <antontsel@yandex.ru>
2020-05-19 20:55:15 +0400

    Update dispatcher context. Now server can start

--- src/main/java/com/tsel/app/App.java
+++ src/main/java/com/tsel/app/App.java
@@ -1,6 +1,5 @@
 package com.tsel.app;
 
-import com.tsel.app.service.TimeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
@@ -11,28 +10,30 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import java.time.format.DateTimeFormatter;
 
 @Slf4j
 public class App implements WebApplicationInitializer {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-
     @Override
     public void onStartup(ServletContext container) throws ServletException {
 
-        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-        appContext.setConfigLocation(
-//                "file:src/main/webapp/WEB-INF/dispatcher-config.xml");
-                "file:src/main/resources/contexts/main-context.xml");
+        XmlWebApplicationContext mainContext = new XmlWebApplicationContext();
+        mainContext.setConfigLocation("file:src/main/resources/contexts/main-context.xml");
 
+        // Передаём управление жизненным циклом корневого контекста Сервлет контексту
+        container.addListener(new ContextLoaderListener(mainContext));
+
+        // Создаём контекст для dispatcher servlet'а
+        AnnotationConfigWebApplicationContext dispatcherContext =
+                new AnnotationConfigWebApplicationContext();
+        dispatcherContext.scan("com.epam.rd");
+
+        // Регистрируем dispatcher servlet
         ServletRegistration.Dynamic dispatcher =
-                container.addServlet("dispatcher", new DispatcherServlet(appContext));
+                container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
 
-        TimeService service = appContext.getBean("timeService", TimeService.class);
         log.info("App started");
-        log.info("Program time: " + service.now().format(FORMATTER));
     }
 }
